e74161af71680379e1ebad4b846faec45ee62da2
Megh <meghavarshini@gmail.com>
2021-10-05 20:05:33 -0700

    clean code

--- ggimage_gradient_norm_meme.R
+++ ggimage_gradient_norm_meme.R
@@ -4,12 +4,26 @@ library("ggimage")
 # create a df
 
 #set.seed(2017-02-21)
-x <- 1:10
-y <- c(1.5, 1.2, 1.3, 1.7, 8, 1.2, 1.5, 1.4, 1.5, 2)
-d <- data.frame(x, y,
-                image = sample(c("https://www.github.com/gradient-norm-meme/doomer.png",
-                                 "https://www.github.com/gradient-norm-meme/Surprised_Pikachu.png"),
-                               size=13, replace = TRUE)
-)
+trainig_step <- 1:10
+grad_norm <- c(1.5, 1.2, 1.3, 1.7, 8, 1.8, 1.8, 1.9, 1.7, 2)
+d <- data.frame(trainig_step, grad_norm,
+                image = c("doomer.png",
+                                 "doomer.png",
+                                 "doomer.png",
+                                 "doomer.png",
+                                 "Surprised_Pikachu.png",
+                                 "doomer.png",
+                                 "doomer.png",
+                                 "doomer.png",
+                                 "doomer.png",
+                                 "doomer.png"))
+
 # plot2
-ggplot(d, aes(x, y)) + geom_image(aes(image=image), size=.05)+ ylim(1,10)+xlim(0,11)
+ggplot(d, aes(trainig_step, grad_norm)) + geom_image(aes(image=image), size=.13)+ ylim(1,10)+xlim(0,11)+
+  theme(
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title=element_text(size=24,face="bold")
+    )+ xlab("Training Steps") + ylab("Grad Norm")
+
